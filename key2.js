a174d1b5617d48bc5fda73f142f4c5b9bbb32d26
阿男 <l.weinan@gmail.com>
2020-01-17 20:53:41 +0800

    .

--- key2.js
+++ key2.js
@@ -1,5 +1,20 @@
 nodeRSA = require("node-rsa");
 
+const pubKey = new nodeRSA("-----BEGIN PUBLIC KEY-----\n" +
+    "MIIBYjANBgkqhkiG9w0BAQEFAAOCAU8AMIIBSgKCAUEAsE1edyfToZRv6cFOkB0t\n" +
+    "AJ5qJor4YF5CccJAL0fS/o1Yk10VSXH4Xx4peSJgYQKkO0HqO1hAz6k9dFQB4U1C\n" +
+    "nWtRjtNEcIfycqrZrhu6you5syb6ScV3Zu/9bm7/DyaLlx/gJhUPR1OxOzaqsEvl\n" +
+    "u7hbDhNLIYo1zKFb/aUBbD6+UcaGxH2BfFNdzVAtVSVpc/s2Y3sboMN7rByUj793\n" +
+    "7iQlaMINvVjyasynYuzHNw6ZRP9JP9fwxrCyaxnTPWxVl0qvVaQO2+TtFMtDXH2O\n" +
+    "VZtWWeLHAL8cildw0G+u2qVqTqIGEwNyJlsAHykaPFAMW0xLueumrSlB+JUJPrRv\n" +
+    "vw4nBCd4GOrNSlPCE/xlk1Cb8JaICTLvDUcYc3ZqL3jqAueBhkpw2uCz8xVJeOA1\n" +
+    "KY4kQIIx8JEBsAYzgyP2iy0CAwEAAQ==\n" +
+    "-----END PUBLIC KEY-----");
+
+console.log(pubKey.encrypt("Hello, world").toJSON());
+// 公钥没法做私钥加密
+// 但是私钥可以既做公钥加密也可以做私钥加密
+// console.log(pubKey.encryptPrivate("Hello, world")); <-- 这个会报错
 
 // 有私钥可以生成公钥
 const key = new nodeRSA('-----BEGIN RSA PRIVATE KEY-----\n'+
@@ -17,22 +32,4 @@ const key = new nodeRSA('-----BEGIN RSA PRIVATE KEY-----\n'+
 console.log(key.encrypt("Hello, world!").toJSON());
 // 公钥和私钥加密出来的数据是不一样的
 console.log(key.encryptPrivate("Hello, world!").toJSON());
-
-
-const pubKey = new nodeRSA('-----BEGIN PUBLIC KEY-----\n' +
-    'MIIBYjANBgkqhkiG9w0BAQEFAAOCAU8AMIIBSgKCAUEAsE1edyfToZRv6cFOkB0t\n' +
-    'AJ5qJor4YF5CccJAL0fS/o1Yk10VSXH4Xx4peSJgYQKkO0HqO1hAz6k9dFQB4U1C\n' +
-    'nWtRjtNEcIfycqrZrhu6you5syb6ScV3Zu/9bm7/DyaLlx/gJhUPR1OxOzaqsEvl\n' +
-    'u7hbDhNLIYo1zKFb/aUBbD6+UcaGxH2BfFNdzVAtVSVpc/s2Y3sboMN7rByUj793\n' +
-    '7iQlaMINvVjyasynYuzHNw6ZRP9JP9fwxrCyaxnTPWxVl0qvVaQO2+TtFMtDXH2O\n' +
-    'VZtWWeLHAL8cildw0G+u2qVqTqIGEwNyJlsAHykaPFAMW0xLueumrSlB+JUJPrRv\n' +
-    'vw4nBCd4GOrNSlPCE/xlk1Cb8JaICTLvDUcYc3ZqL3jqAueBhkpw2uCz8xVJeOA1\n' +
-    'KY4kQIIx8JEBsAYzgyP2iy0CAwEAAQ==\n' +
-    '-----END PUBLIC KEY-----');
-
-console.log(pubKey.encrypt("Hello, world").toJSON());
-// 公钥没法做私钥加密
-// 但是私钥可以既做公钥加密也可以做私钥加密
-console.log(pubKey.encryptPrivate("Hello, world"));
-
-
+console.log(key.decrypt(pubKey.encrypt("Hello, Martian!").buffer).toJSON());
